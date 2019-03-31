aae9966a7fa751cd210d469071735c0dc6237df0
Fahad Azhar <fahad.azhar@tenpearls.com>
2019-04-01 01:38:26 +0500

    changes

--- keys/index.js
+++
@@ -1,3 +0,0 @@
-import fs from 'fs';
-const publicKey = fs.readFileSync('keys/foundationApp.pem');
-export default publicKey;
--- secret-bucket.js
+++
@@ -1,6 +0,0 @@
-export const awsConfig = {
-  'access_id': 'AKIAIIEOVLMVXVNNEXFA',
-  'access_key': 'qZSBUh1QZIwUDu2G0BmYFkj4qlD2iTfeSQYEpuvg',
-  's3Path': 'https://s3.eu-central-1.amazonaws.com/selfmade.city/',
-  'bucket': 'selfmade.city'
-}
